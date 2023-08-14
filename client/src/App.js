2b21e7d700b6d65636de77030ad6cf98772a27d6
Wihctoh <rivertoxi911@gmail.com>
2023-08-14 13:50:26 +0300

    add dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegPage from "./pages/RegPage/RegPage";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { darkTheme } from "./Theme";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/registration" element={<RegPage />}></Route>
-      </Routes>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/registration" element={<RegPage />}></Route>
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ client/src/Theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+// const colorObj = {
+//   cerulean: {
+//     50: "#f0faff",
+//     100: "#e0f5fe",
+//     200: "#bae8fd",
+//     300: "#7dd5fc",
+//     400: "#38bcf8",
+//     500: "#0ea5e9",
+//     600: "#028ac7",
+//     700: "#0370a1",
+//     800: "#075e85",
+//     900: "#0c506e",
+//     950: "#083549",
+//   },
+// };
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
